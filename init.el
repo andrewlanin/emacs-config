5b11492567d9e31847e861c2d09d759f8c1f67b0
Andrew Lanin <andrew@lanin.dev>
2021-03-24 15:50:53 +0300

    Remove default treemacs config.

--- init.el
+++ init.el
@@ -136,7 +136,7 @@
   :config
     (counsel-mode 1)
     (global-set-key (kbd "C-S-f") 'counsel-rg)
-    (global-set-key (kbd "C-b") 'counsel-switch-buffer))
+    (global-set-key (kbd "C-b")   'counsel-switch-buffer))
 
 ;; Swiper does interactive search inside current file.
 (use-package swiper
@@ -178,10 +178,10 @@
     (global-set-key (kbd "<M-down>")  'windmove-down))
 
 ;; Change windows size.
-(global-set-key (kbd "M-S-<left>") 'shrink-window-horizontally)
+(global-set-key (kbd "M-S-<left>")  'shrink-window-horizontally)
 (global-set-key (kbd "M-S-<right>") 'enlarge-window-horizontally)
-(global-set-key (kbd "M-S-<down>") 'shrink-window)
-(global-set-key (kbd "M-S-<up>") 'enlarge-window)
+(global-set-key (kbd "M-S-<down>")  'shrink-window)
+(global-set-key (kbd "M-S-<up>")    'enlarge-window)
 
 ;; -----------------------------------------------------------------------------
 ;; Org mode.
@@ -198,44 +198,8 @@
 
 (use-package treemacs
   :config
-  (setq treemacs-collapse-dirs                 (if treemacs-python-executable 3 0)
-        treemacs-deferred-git-apply-delay      0.5
-        treemacs-directory-name-transformer    #'identity
-        treemacs-display-in-side-window        t
-        treemacs-eldoc-display                 t
-        treemacs-file-event-delay              5000
-        treemacs-file-extension-regex          treemacs-last-period-regex-value
-        treemacs-file-follow-delay             0.2
-        treemacs-file-name-transformer         #'identity
-        treemacs-follow-after-init             t
-        treemacs-git-command-pipe              ""
-        treemacs-goto-tag-strategy             'refetch-index
-        treemacs-indentation                   2
-        treemacs-indentation-string            " "
-        treemacs-is-never-other-window         nil
-        treemacs-max-git-entries               5000
-        treemacs-missing-project-action        'ask
-        treemacs-no-png-images                 t
-        treemacs-no-delete-other-windows       t
-        treemacs-project-follow-cleanup        nil
-        treemacs-persist-file                  (expand-file-name "treemacs-workspaces" user-emacs-directory)
-        treemacs-position                      'left
-        treemacs-recenter-distance             0.1
-        treemacs-recenter-after-file-follow    nil
-        treemacs-recenter-after-tag-follow     nil
-        treemacs-recenter-after-project-jump   'always
-        treemacs-recenter-after-project-expand 'on-distance
-        treemacs-show-cursor                   nil
-        treemacs-show-hidden-files             t
-        treemacs-silent-filewatch              nil
-        treemacs-silent-refresh                nil
-        treemacs-sorting                       'alphabetic-asc
-        treemacs-space-between-root-nodes      t
-        treemacs-tag-follow-cleanup            t
-        treemacs-tag-follow-delay              1.5
-        treemacs-user-mode-line-format         nil
-        treemacs-width                         35
-        )
+  (setq treemacs-no-png-images t
+        treemacs-persist-file (expand-file-name "treemacs-workspaces" user-emacs-directory))
   (treemacs-follow-mode t)
   (treemacs-filewatch-mode t)
   (treemacs-fringe-indicator-mode t)
